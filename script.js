ebbd80de2eff16ec347aced9d86d852161d08ab0
Aakash52thade <117812701+Aakash52thade@users.noreply.github.com>
2025-08-01 10:56:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+function updateClock() {
+
+	const now = new Date();
+	const formateTime = now.toLocalString();
+	document.getElementById('timer').textContext = formateTime;
+	
+}
+updateClock();
